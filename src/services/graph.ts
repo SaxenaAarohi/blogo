bd93ebfbb80e8321976c6af3d2c9b59a92ad8868
AarohiSaxena10 <aarohisaxena1014@gmail.com>
2025-08-19 10:39:00 +0530

    2ndcommit

--- src/services/graph.ts
+++ src/services/graph.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from "graphql-request";
-const gqlclient = new GraphQLClient("https://blogo-nu-pearl.vercel.app/api/graphql");
+const gqlclient = new GraphQLClient("https://blogo-main.vercel.app/api/graphql");
 export default gqlclient;
 
 
